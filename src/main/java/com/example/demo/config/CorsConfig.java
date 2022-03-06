e60cf254175ba7ec3a3d52a5d7e5352c50974c99
adelalira <aalira.96@gmail.com>
2022-03-06 20:22:16 +0100

    enlaces

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -14,10 +14,12 @@ public class CorsConfig implements WebMvcConfigurer {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 
+				String link = "https://adelalira.github.io/";
+				
 				/**
 				 * CORS PARA PODER HACER LOGIN
 				 */
-				registry.addMapping("/auth/login").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/auth/login").allowedOrigins(link)
                 .allowedHeaders("GET", "POST", "DELETE", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
                        "Authorization", "Access-Control-Request-Headers")
                 .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
@@ -25,7 +27,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				/**
 				 * CORS PARA PODER HACER UN REGISTRO DE UN USUARIO
 				 */
-				 registry.addMapping("/auth/register").allowedOrigins("http://localhost:4200")
+				 registry.addMapping("/auth/register").allowedOrigins(link)
 	                .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                        "Access-Control-Request-Headers")
 	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
@@ -33,7 +35,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				/**
 				 * CORS PARA PODER COMPROBAR EL TOKEN
 				 */
-				 registry.addMapping("/checkToken").allowedOrigins("http://localhost:4200")
+				 registry.addMapping("/checkToken").allowedOrigins(link)
 	                .allowedHeaders("GET", "POST", "DELETE", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                       "Authorization", "Access-Control-Request-Headers")
 	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
@@ -41,7 +43,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				 /**
 				  * CORS PARA MOSTRAR TODOS LOS SERVICIOS DE LA APLICACIÓN
 				  */
-				 registry.addMapping("/servicios").allowedOrigins("https://adelalira.github.io/")
+				 registry.addMapping("/servicios").allowedOrigins(link)
 	                .allowedHeaders("GET", "POST", "DELETE", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                       "Authorization", "Access-Control-Request-Headers")
 	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
@@ -49,7 +51,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				/**
 				 * CORS PARA COMPROBAR SI EN LA BASE DE DATOS ESTA EL EMAIL QUE INDICAMOS
 				 */
-				 registry.addMapping("/auth/user/email/{email}").allowedOrigins("http://localhost:4200")
+				 registry.addMapping("/auth/user/email/{email}").allowedOrigins(link)
 	                .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "Authorization", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                        "Access-Control-Request-Headers")
 	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
@@ -57,7 +59,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				/**
 				 * CORS PARA MOSTRAR LOS DATOS DEL USUARIO
 				 */
-				 registry.addMapping("/auth/user").allowedOrigins("http://localhost:4200")
+				 registry.addMapping("/auth/user").allowedOrigins(link)
 				 .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "Authorization", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                        "Access-Control-Request-Headers")
 	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
@@ -65,7 +67,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				/**
 				 * CORS PARA MOSTRAR LOS DATOS DEL USUARIO
 				 */
-				 registry.addMapping("/user").allowedOrigins("http://localhost:4200")
+				 registry.addMapping("/user").allowedOrigins(link)
 				 .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "Authorization", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                        "Access-Control-Request-Headers")
 	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
@@ -73,7 +75,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				/**
 				 * CORS PARA AÑADIR CITAS
 				 */
-				 registry.addMapping("/cita").allowedOrigins("http://localhost:4200")
+				 registry.addMapping("/cita").allowedOrigins(link)
 				 .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "Authorization", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                        "Access-Control-Request-Headers")
 	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
@@ -81,7 +83,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				/**
 				 * CORS PARA MOSTRAR LA CITA INDICADA
 				 */
-				 registry.addMapping("/cita/{idC}").allowedOrigins("http://localhost:4200")
+				 registry.addMapping("/cita/{idC}").allowedOrigins(link)
 				 .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "Authorization", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                        "Access-Control-Request-Headers")
 	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
@@ -89,7 +91,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				/**
 				 * CORS PARA ELIMINAR LA CITA INDICADA
 				 */
-				 registry.addMapping("/cita/{idC}/delete").allowedOrigins("http://localhost:4200")
+				 registry.addMapping("/cita/{idC}/delete").allowedOrigins(link)
 				 .allowedMethods("GET", "POST", "OPTIONS", "PUT","DELETE")
 				 .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "Authorization", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                        "Access-Control-Request-Headers")
@@ -98,7 +100,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				/**
 				 * CORS PARA AÑADIR A UNA CITA UN SERVICIO
 				 */
-				 registry.addMapping("/cita/{idC}/lineaCitaServicio").allowedOrigins("http://localhost:4200")
+				 registry.addMapping("/cita/{idC}/lineaCitaServicio").allowedOrigins(link)
 				 .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "Authorization", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                        "Access-Control-Request-Headers")
 	                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
@@ -107,7 +109,7 @@ public class CorsConfig implements WebMvcConfigurer {
 				/**
 				 * CORS PARA QUE SE PUEDA ENVIAR UN EMAIL
 				 */
-				 registry.addMapping("/sendMail").allowedOrigins("http://localhost:4200")
+				 registry.addMapping("/sendMail").allowedOrigins(link)
 				 .allowedMethods("GET", "POST", "OPTIONS", "PUT","DELETE")
 				 .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "Authorization", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 	                        "Access-Control-Request-Headers")
